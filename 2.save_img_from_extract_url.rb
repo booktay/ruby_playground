278b13d3cff94198b04627815a4165eb544d1408
Va1kar1e <siwanont1@gmail.com>
2021-01-21 19:08:24 +0700

    Update 2.save_img_from_extract_url.rb

--- 2.save_img_from_extract_url.rb
+++ 2.save_img_from_extract_url.rb
@@ -3,13 +3,20 @@ require "nokogiri"
 require 'fileutils'
 
 urls = [
+  ""
 ]
+
+directory = "opc"
+destination_path = "result/" + directory + "/"
+unless File.directory?(destination_path)
+  FileUtils.mkdir_p(destination_path)
+end
 
 urls.each do |url|
     puts "Start : " + url
 
     # Get Source html from url
-    doc = Nokogiri::HTML(open(url))
+    doc = Nokogiri::HTML(URI.open(url))
 
     # Get img urls list
     search_keyword = 'div#image-container img'
@@ -18,25 +25,21 @@ urls.each do |url|
     # Map URLs
     image_urls = doc.css(search_keyword).map { |link| link[filter_keyword] }
     
-    puts "Number of images : " + image_urls.length.to_s
+    puts "Number of images : %03d" % [image_urls.length]
 
-    directory = "opc"
     episode = url.split('/').last.tr('^0-9', '')
-    destination_path = "result/" + directory + "/"
-    
-    unless File.directory?(destination_path)
-      FileUtils.mkdir_p(destination_path)
-    end
-
+  
     # Save Results Url to file
     image_urls.each_with_index do |imgurl, index|
-      name = directory + "_" + episode + "_" + (index+1).to_s + ".jpg"
+      name = "%s_%d_%03d.jpg" % [directory, episode, (index+1)]
 
-      open(imgurl) do |u|
+      URI.open(imgurl) do |u|
         File.open(destination_path + name, 'wb') { |f| f.write(u.read) }
       end
 
-      puts "[" + (index+1).to_s + "/" + image_urls.length.to_s + "] " + name
+      if (index + 1) == 1 or (index + 1) == image_urls.length or (index + 1) % 5 == 0
+        puts "[%03d/%03d] %s" % [(index+1), image_urls.length, name]
+      end
     end
     puts "Complete : " + url
 end
